0cb1a20a42393ca15047996e6e644feee803df63
mohdabdin <72082608+mohdabdin@users.noreply.github.com>
2021-03-07 22:21:47 +0000

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -11,16 +11,20 @@ from cnfGenerator import cnfGenerator
 
 tf.compat.v1.enable_v2_behavior()
 
+CLAUSE_LENGTH = 2
+N_VARIABLES = 10
+CNF_SIZE = 40
+
 class CnfSolverEnv(py_environment.PyEnvironment):
   def __init__(self):
     self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=40, name='action')
+        shape=(), dtype=np.int32, minimum=0, maximum=(N_VARIABLES*2)+1, name='action')
     self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(1,), dtype=np.int32, minimum=0, name='observation')
-    self.generator = cnfGenerator(n_variables = 10, clause_range=(1,3), cnf_size=40)
+        shape=(40,), dtype=np.int32, minimum=[-N_VARIABLES for _ in range(CNF_SIZE)], maximum=[N_VARIABLES for _ in range(CNF_SIZE)], name='observation')
+    self.generator = cnfGenerator(n_variables = N_VARIABLES, clause_range=(2,3), cnf_size=40)
     self.cnf = self.generator.generateCnf()
     self.initial_label = self.generator.solveCnf(self.cnf)
-    self._state = self.cnf
+    self._state = np.array(self.cnf)
     self._episode_ended = False
 
   def action_spec(self):
@@ -31,58 +35,86 @@ class CnfSolverEnv(py_environment.PyEnvironment):
 
   def _reset(self):
     self.cnf = self.generator.generateCnf()
-    self._state = self.cnf
+    self._state = np.array(self.cnf)
     self._episode_ended = False
-    return ts.restart(self._state)
+    return ts.restart(self._state.flatten())
 
-  def simplify(self, lit):
+  def simplify(self, cnf, lit):
     c=0
-    while c<len(self.cnf):
-      if -lit in self.cnf[c]:
-        self.cnf[c].remove(-lit)
-      if not self.cnf[c] or lit in self.cnf[c]:
-        self.cnf.remove(self.cnf[c])
+    while c<len(cnf):
+      l=0
+      while l<len(cnf[c]):
+        if -lit==cnf[c][l]:
+          cnf[c][l] = 0
+        l+=1
+      if lit in cnf[c]:
+        cnf[c] = [0 for _ in range(CLAUSE_LENGTH)]
       c+=1
-    return self.cnf
+
+    return cnf
+
+  def minisat_reformat(self, cnf):
+    #removes the zeros from the cnf to be understood by the minisat solver
+    cnf = cnf.tolist()
+    for clause in cnf:
+        i=0
+        while i<len(clause):
+            if clause[i]==0:
+                clause.remove(0)
+                i-=1
+            i+=1
+    return [x for x in cnf if len(x)>0]
+  
+      
   
   def _step(self, action):
     reward = 0
-    
+    action = action-N_VARIABLES
     if action==0:
         #agent predicts unsatifiable
         if self.initial_label == True:
             reward = -1 #negative reward for false prediction
-            print("Agent predicted unsatisfiable incorrectly")
+            print("episode ended: Agent predicted unsatisfiable incorrectly")
         else:
             reward = 1 #positive reward for correct prediction
-            print("Agent predicted unsatisfiable correctly")
+            print("episode ended: Agent predicted unsatisfiable correctly")
             
         self._episode_ended = True
-        return ts.termination(self._state, reward)
+        return ts.termination(self._state.flatten(), reward)
     
-    elif action==99:
+    elif action==N_VARIABLES+1:
         #agent predicts satisfiable
         if self.initial_label == False:
             reward = -1 #negative reward for false prediction
-            print("Agent predicted satisfiable incorrectly")
+            print("episode ended: Agent predicted satisfiable incorrectly")
         else:
             reward = 1 #positive reward for correct prediction
-            print("Agent predicted satisfiable correctly")
+            print("episode ended: Agent predicted satisfiable correctly")
             
         self._episode_ended = True
-        return ts.termination(self._state, reward)
+        return ts.termination(self._state.flatten(), reward)
+    
+    elif action not in self._state or -action not in self._state:
+        #if literal in not present in formula
+        reward = -1
+        print("agent chose a literal not found in cnf")
+        
+        self._episode_ended = True
+        return ts.termination(self._state.flatten(), reward)
     
     else:
-        #agent chosen a literal to simplify the formula
-        updated_cnf = self.simplify(action)   
-        if self.generator.solveCnf(updated_cnf) != self.initial_label:
+        #agent chose a literal to simplify the formula
+        self._state = self.simplify(self._state, action)   
+        minisat_updated_cnf = self.minisat_reformat(self._state)
+        
+        if self.generator.solveCnf(minisat_updated_cnf) != self.initial_label:
+            print("episode ended: Agent simplified the cnf making it unsatisfiable when it was satisfiable")
             #if the agent's implification made it unsatifiable when it was satisfiable
             self._episode_ended = True
             reward = -1
-            return ts.termination(self._state, reward)
+            return ts.termination(self._state.flatten(), reward)
 
-    self._state = updated_cnf
-    return ts.transition(self._state, reward, discount=1.0)
+    return ts.transition(self._state.flatten(), reward, discount=1.0)
     
 
   def current_time_step(self):
@@ -90,16 +122,3 @@ class CnfSolverEnv(py_environment.PyEnvironment):
 
   def render(self):
     print(self._state)
-
-
-
-
-
-
-
-
-
-
-
-
-
