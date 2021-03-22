ce3e6e207d7ca5698d1d86e0b4ed905d2e7f2466
AutumnHaunted <74358771+AutumnHaunted@users.noreply.github.com>
2021-03-22 17:30:00 +0000

    Update cnfGenerator.py

--- cnfGenerator.py
+++ cnfGenerator.py
@@ -12,33 +12,26 @@ class cnfGenerator:
 
     def generateCnf(self):
         bias = random.random()
-        bias_satisfied = False
-        while not bias_satisfied:
-            cnf = []
-            i = 0
-            while i<self.cnf_size:
-                clause = [] 
-                #random number of literals in each clause
-                n_clause = random.randrange(self.clause_range[0], self.clause_range[1])
-                for _ in range(n_clause):
-                    lit_state = random.randint(1,2)
-                    lit = random.randint(1,self.n_variables)
-                    if lit_state==1: #positive literal
-                        clause.append(lit)
-                    else: #negated literal
-                        clause.append(-lit)
-                    i+=1
-                cnf.append(clause)
-                
-            sat = self.solveCnf(cnf)
-            if bias<self.sat_bias and sat:
-                #we want a satisfiable formula
-                bias_satisfied = True
-                    
-            elif bias>self.sat_bias and not sat:
-                #we want a unsatisfiable formula
-                bias_satisfied = True
-            
+        if(bias >= self.sat_bias):
+            desiredSat = True
+        else:
+            desiredSat = False
+        cnf = []
+        i = 0
+        while i<self.cnf_size:
+            clause = [] 
+            #random number of literals in each clause
+            n_clause = random.randrange(self.clause_range[0], self.clause_range[1])
+            for _ in range(n_clause):
+                lit_state = random.randint(1,2)
+                lit = random.randint(1,self.n_variables)
+                if lit_state==1: #positive literal
+                    clause.append(lit)
+                else: #negated literal
+                    clause.append(-lit)
+                i+=1
+            cnf.append(clause)
+        cnf = self.forceSatOrUnsat(cnf, desiredSat)
         return cnf
 
     def solveCnf(self, cnf):
@@ -52,10 +45,14 @@ class cnfGenerator:
         label = self.solveCnf(cnf)
         return {"cnf": cnf,
                 "label": label}
-
     
+    def forceSatOrUnsat(self, cnf, desiredSat):
+        while(self.solveCnf(cnf) != desiredSat):
+            clauseNum = int(random.random() * len(cnf)) - 1
+            cnf[clauseNum][int(random.random() * len(cnf[clauseNum])) - 1] *= -1
+        return cnf
     
-    
-    
-    
-   
+g = cnfGenerator()
+for i in range(10):
+    cnf = g.generateCnf()
+    print(g.solveCnf(cnf))
