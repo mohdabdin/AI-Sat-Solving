0966e0b86486f2ebb604d36a24c6b37abf53ade4
Mohammad Abdin <72082608+mohdabdin@users.noreply.github.com>
2021-03-28 18:07:18 +0100

    Update cnfGenerator.py

--- cnfGenerator.py
+++ cnfGenerator.py
@@ -3,7 +3,7 @@ import random
 from pysat.solvers import Solver
 
 class cnfGenerator:
-    def __init__(self, n_variables=4, clause_range = (3,4), cnf_size=20, sat_bias=0):
+    def __init__(self, n_variables=2, clause_range = (2,3), cnf_size=6, sat_bias=0.5):
        self.n_variables = n_variables
        self.clause_range = clause_range
        self.cnf_size = cnf_size
@@ -15,22 +15,29 @@ class cnfGenerator:
             desiredSat = True
         else:
             desiredSat = False
-        cnf = []
-        i = 0
-        while i<self.cnf_size:
-            clause = [] 
-            #random number of literals in each clause
-            n_clause = random.randrange(self.clause_range[0], self.clause_range[1])
-            for _ in range(n_clause):
-                lit_state = random.randint(1,2)
-                lit = random.randint(1,self.n_variables)
-                if lit_state==1: #positive literal
-                    clause.append(lit)
-                else: #negated literal
-                    clause.append(-lit)
-                i+=1
-            cnf.append(clause)
-        cnf = self.forceSatOrUnsat(cnf, desiredSat)
+            
+        done = False
+        while not done:
+            cnf = []
+            i = 0
+            while i<self.cnf_size:
+                clause = [] 
+                #random number of literals in each clause
+                n_clause = random.randrange(self.clause_range[0], self.clause_range[1])
+                for _ in range(n_clause):
+                    lit_state = random.randint(1,2)
+                    lit = random.randint(1,self.n_variables)
+                    if lit_state==1: #positive literal
+                        clause.append(lit)
+                    else: #negated literal
+                        clause.append(-lit)
+                    i+=1
+                cnf.append(clause)
+                
+            if desiredSat==self.solveCnf(cnf):
+                done=True
+                
+        #cnf = self.forceSatOrUnsat(cnf, desiredSat)
         return cnf
 
     def solveCnf(self, cnf):
@@ -42,16 +49,5 @@ class cnfGenerator:
         label = self.solveCnf(cnf)
         return {"cnf": cnf,
                 "label": label}
-    
-    def forceSatOrUnsat(self, cnf, desiredSat):
-        while(self.solveCnf(cnf) != desiredSat):
-            clauseNum = int(random.random() * len(cnf)) - 1
-            cnf[clauseNum][int(random.random() * len(cnf[clauseNum])) - 1] *= -1
-            #print(self.solveCnf(cnf))
-            #print(desiredSat)
-            #print("-----")
-        return cnf
-    
-    
     
     
